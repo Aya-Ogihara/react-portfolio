0759928bfd349f77c9c929644896c9360bf15b79
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-06 11:53:31 +1300

    ✨ Create top section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { BsMoonStarsFill } from 'react-icons/bs';
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+} from 'react-icons/ai';
+
+//image asset
+import profileImage from '../public/dev-ed-wave.png';
 
 export default function Home() {
   return (
@@ -11,19 +19,47 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
+      <main className=' bg-white px-10'>
         <section className='min-h-screen'>
-          <nav>
-            <h1>Aya Ogihara</h1>
-            <ul>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>Aya Ogihara</h1>
+            <ul className='flex items-center'>
               <li>
-                <BsMoonStarsFill />
+                <BsMoonStarsFill className='cursor-pointer text-2xl' />
               </li>
               <li>
-                <a href='#'>Resume</a>
+                <a
+                  href='#'
+                  className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-cyan-500 font-medium'>
+              Aya Ogihara
+            </h2>
+            <h3 className='text-2xl py-2'>Design focused Frontend Developer</h3>
+            <p className='text-medium py-5 leading-8 text-gray-800'>
+              I enjoy keeping up to date with the evolving changes in the world
+              of Frontend Development.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image
+              src={profileImage}
+              alt='Aya'
+              layout='fill'
+              objectFit='cover'
+            />
+          </div>
         </section>
       </main>
     </>
